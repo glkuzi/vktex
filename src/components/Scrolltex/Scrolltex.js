07be64175c596dbd4de399d231a4f56393e05021
KirillRnd <kirill_rnd@mail.ru>
2019-09-27 20:42:40 +0300

    croll-items

--- src/components/Scrolltex/Scrolltex.js
+++ src/components/Scrolltex/Scrolltex.js
@@ -1,9 +1,51 @@
 import React from 'react';
 import './Scrolltex.css';
+import { HorizontalScroll,  Group} from '@vkontakte/vkui';
+
+const itemStyle = {
+    
+    width: 40,
+    height: 40,
+    display: 'flex',
+    flexDirection:
+    'column',
+    alignItems: 'center',
+    fontSize: 24,
+	backgroundColor: 'grey',
+	margin: 4,
+	padding: 2
+  };
 
 class Scrolltex extends React.Component {
    render() {
-     return <div>This is a component called Scrolltex.</div>;
+     return  <Group style={{ paddingBottom: 8 }}> <HorizontalScroll>
+          <div style={{ display: 'flex' }}>
+            <div style={{ ...itemStyle, paddingLeft: 4 }}>
+              F
+            </div>
+            <div style={itemStyle}>
+              Z
+            </div>
+            <div style={itemStyle}>
+              X
+            </div>
+            <div style={itemStyle}>
+              Th
+            </div>
+			<div style={itemStyle}>
+              A
+            </div>
+            <div style={itemStyle}>
+              Z
+            </div>
+            <div style={itemStyle}>
+              X
+            </div>
+            <div style={itemStyle}>
+              Th
+            </div>
+          </div>
+        </HorizontalScroll> </Group>;
    }
 }
 
