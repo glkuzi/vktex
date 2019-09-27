ff9071d401726bc4048cc8d37bdd6fa0fc758602
KirillRnd <kirill_rnd@mail.ru>
2019-09-27 22:13:59 +0300

    modal buttons

--- src/components/Scrolltex/Scrolltex.js
+++ src/components/Scrolltex/Scrolltex.js
@@ -1,6 +1,97 @@
 import React from 'react';
 import './Scrolltex.css';
-import { HorizontalScroll,  Group, Header} from '@vkontakte/vkui';
+import { HorizontalScroll,  Group, Header,
+PanelHeader,FormLayout, ModalRoot, ModalPage, ModalPageHeader, Button, View, Panel, HeaderButton} from '@vkontakte/vkui';
+import Icon24Cancel from '@vkontakte/icons/dist/24/cancel';
+import Icon24Done from '@vkontakte/icons/dist/24/done';
+import Icon24Dismiss from '@vkontakte/icons/dist/24/dismiss';
+const MODAL_PAGE_SYMBOLS = 'symbols';
+const MODAL_PAGE_EQUATIONS = 'equations';
+
+class Scrolltexmodal extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      activeModal: null,
+      modalHistory: []
+    };
+
+    this.modalBack = () => {
+      this.setActiveModal(this.state.modalHistory[this.state.modalHistory.length - 2]);
+    };
+  }
+
+  setActiveModal(activeModal) {
+    activeModal = activeModal || null;
+    let modalHistory = this.state.modalHistory ? [...this.state.modalHistory] : [];
+
+    if (activeModal === null) {
+      modalHistory = [];
+    } else if (modalHistory.indexOf(activeModal) !== -1) {
+      modalHistory = modalHistory.splice(0, modalHistory.indexOf(activeModal) + 1);
+    } else {
+      modalHistory.push(activeModal);
+    }
+
+    this.setState({
+      activeModal,
+      modalHistory
+    });
+  };
+
+  render() {
+    const modal = (
+      <ModalRoot activeModal={this.state.activeModal}>
+        <ModalPage
+          id={MODAL_PAGE_SYMBOLS}
+          onClose={this.modalBack}
+          header={
+            <ModalPageHeader
+              left={<HeaderButton onClick={this.modalBack}><Icon24Cancel /></HeaderButton>}
+            >
+              Символы
+            </ModalPageHeader>
+          }
+        >
+          
+        </ModalPage>
+
+        <ModalPage
+          id={MODAL_PAGE_EQUATIONS}
+          header={
+            <ModalPageHeader
+              left={<HeaderButton onClick={this.modalBack}><Icon24Cancel /></HeaderButton>}
+            >
+              Выражения
+            </ModalPageHeader>
+          }
+          onClose={this.modalBack}
+          settlingHeight={80}
+        >
+        </ModalPage>
+
+       
+      </ModalRoot>
+    );
+
+    return (
+	<div modal={modal}>
+	    <Header level="secondary">Инструменты</Header>
+        
+		<HorizontalScroll>
+              <Button size="xl" level="secondary" onClick={() => this.setActiveModal(MODAL_PAGE_SYMBOLS)}>
+                  Открыть символы
+              </Button>
+				<Button size="xl" level="secondary" onClick={() => this.setActiveModal(MODAL_PAGE_EQUATIONS)}>
+                  Открыть выражения
+              </Button>
+		</HorizontalScroll>
+    </div>    
+    );
+  }
+}
+
 
 const itemStyle = {
     
@@ -18,35 +109,14 @@ const itemStyle = {
 
 class Scrolltex extends React.Component {
    render() {
-     return  <Group style={{ paddingBottom: 8 }}> 
-	 <Header level="secondary">Инструменты</Header><HorizontalScroll>
-          <div style={{ display: 'flex' }}>
-            <div style={{ ...itemStyle, paddingLeft: 4 }}>
-              F
-            </div>
-            <div style={itemStyle}>
-              Z
-            </div>
-            <div style={itemStyle}>
-              X
-            </div>
-            <div style={itemStyle}>
-              Th
-            </div>
-			<div style={itemStyle}>
-              A
-            </div>
-            <div style={itemStyle}>
-              Z
-            </div>
-            <div style={itemStyle}>
-              X
-            </div>
-            <div style={itemStyle}>
-              Th
-            </div>
-          </div>
-        </HorizontalScroll> </Group>;
+     return  (
+	 
+	 
+        <Scrolltexmodal></Scrolltexmodal>  
+        
+		 
+		
+		)
    }
 }
 
