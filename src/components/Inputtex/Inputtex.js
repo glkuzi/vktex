6bb8f615748ede8c269ae295b6f8e250bc6a2cf5
KirillRnd <kirill_rnd@mail.ru>
2019-09-28 16:33:57 +0300

    remove $

--- src/components/Inputtex/Inputtex.js
+++ src/components/Inputtex/Inputtex.js
@@ -27,7 +27,7 @@ class Inputtex extends React.Component {
     }
 	
 	onTex(elem) {
-        this.setState({value: this.state.value + elem});
+        this.setState({value: this.state.value + elem.replace(/\$/g, "")});
     }
     render() {
         return (
