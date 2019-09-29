68434d2487c8264f4cfa9647f3936f500d94b332
radiosmersh <radiosmersh@gmail.com>
2019-09-29 04:47:14 +0300

    Remove obsolete method

--- src/components/Inputtex/Inputtex.js
+++ src/components/Inputtex/Inputtex.js
@@ -12,21 +12,6 @@ import axios from 'axios';
 import uuid from 'react-uuid'
 import connect from '@vkontakte/vk-connect'
 const myhref = '#'
-
-function dataURLtoFile(dataurl, filename){
-	const arr = dataurl.split(',')
-	const mime = arr[0].match(/:(.*?);/)[1]
-	const bstr = atob(arr[1])
-	let n = bstr.length
-	const u8arr = new Uint8Array(n)
-	while (n) {
-	  u8arr[n] = bstr.charCodeAt(n)
-	  n -= 1 // to make eslint happy
-	}
-	return new File([u8arr], filename, { type: mime })
-}
-
-
 const maxHash = 1000000;
 const appLink = "https://vk.com/app7150582";
 
